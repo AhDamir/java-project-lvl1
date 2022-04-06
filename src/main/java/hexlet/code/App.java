8d1affbb3595f9df36c56da336efd2c1145710f8
Damir Akhmerov <searchingtalib@yahoo.com>
2022-04-06 15:31:12 +0300

    5 step of 1 project

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,9 +1,68 @@
 package hexlet.code;
-import static hexlet.code.Cli.getNameAndGreetings;
+
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.getNameAndGreetings();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Your choice: ");
+        int gameNumber = scanner.nextInt();
+        switch (gameNumber) {
+
+            case 1:
+                System.out.println("Welcome to the Brain Games!");
+                Cli.getNameAndGreetings();
+                break;
+            case 2:
+                System.out.println("Welcome to the Brain Games!");
+                Cli.getNameAndGreetings();
+                even();
+                break;
+            case 0:
+                System.out.println("See you!");
+                return;
+        }
+
+    }
+    public static  void even() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Question: ");
+            int number = randomNumber();
+            System.out.println(number);
+            System.out.print("Your answer: ");
+            String answer = scanner.nextLine();
+
+            if (number % 2 == 0) {
+                if (answer.equals("yes")) {
+                    System.out.println("Correct!");
+                } else {
+                    System.out.println("'" + answer + "'" + " is wrong answer ;(. Correct answer was 'yes'.");
+                    System.out.println("Let's try again, " + Cli.userName);
+                    return;
+                }
+            } else {
+                if (answer.equals("no")) {
+                    System.out.println("Correct!");
+                } else {
+                    System.out.println("'" + answer + "'" + " is wrong answer ;(. Correct answer was 'no'.");
+                    System.out.println("Let's try again, " + Cli.userName);
+                    return;
+                }
+            }
+        }
+        System.out.println("Congratulations, " + Cli.userName + "!");
+    }
+    public static int randomNumber()
+    {
+        int min = 1;
+        int max = 100;
+        max -= min;
+        return (int) (Math.random() * ++max) + min;
     }
 }
--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,10 +3,11 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static  void getNameAndGreetings () {
+    public static String userName;
+    public static  void getNameAndGreetings() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("May I have your name? ");
-        String userName = scanner.nextLine();
+        userName = scanner.nextLine();
         System.out.println("Hello, " + userName + "!");
     }
 }
