25fdaf3984557b96a959c65a8893abe0f807270d
Damir Akhmerov <searchingtalib@yahoo.com>
2022-04-22 13:56:50 +0300

    fix Even.java

--- src/main/java/hexlet/code/games/Even.java
+++ src/main/java/hexlet/code/games/Even.java
@@ -2,30 +2,37 @@ package hexlet.code.games;
 
 import hexlet.code.Engine;
 
+import java.util.Random;
+
 public class Even {
-    public static  void even(String userName) {
-        final int correctAnswerCounter = 3;
-        for (int i = 0; i < correctAnswerCounter; i++) {
-            final int upperRandomLimit = 20;
-            int dataForQuestion = Engine.generateRandomNumber(upperRandomLimit);
-            Engine.askQuestion(String.valueOf(dataForQuestion));
-            String answer = Engine.getAnswer();
-            String result;
-            if (isEven(dataForQuestion)) {
-                result = "yes";
+    public static  void run() {
+        String gameDescription = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+        final int numberOfQuestions = 3;
+
+        String[][] dataForQuestion = new String[numberOfQuestions][2];
+        for (var i = 0; i < numberOfQuestions; i++) {
+            var question = generateRandomNumber();
+            String correctAnswer;
+            if (isEven(question)) {
+                correctAnswer = "yes";
             } else {
-                result = "no";
+                correctAnswer = "no";
             }
-            if (answer.equals(result)) {
-                Engine.getCorrectAnswer();
-            } else {
-                Engine.getWrongAnswer(answer, result, userName);
-                return;
-            }
+            dataForQuestion[i][0] = String.valueOf(question);
+            dataForQuestion[i][1] = correctAnswer;
         }
-        Engine.getCongratulations(userName);
+
+        Engine.run(gameDescription, dataForQuestion);
     }
+
     public static boolean isEven(int dataForQuestion) {
         return dataForQuestion % 2 == 0;
     }
+
+    public static int generateRandomNumber() {
+        Random random = new Random();
+        final int upperRandomLimit = 20;
+        return random.nextInt(upperRandomLimit) + 1;
+    }
 }
+
