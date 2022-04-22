38cd4513abcd4960faa46a47b7c07aaa903f3d55
Damir Akhmerov <searchingtalib@yahoo.com>
2022-04-22 13:55:16 +0300

    fix App.java

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -12,53 +12,24 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
-        final int item1 = 1;
         System.out.println("2 - Even");
-        final int item2 = 2;
         System.out.println("3 - Calc");
-        final int item3 = 3;
         System.out.println("4 - GCD");
-        final int item4 = 4;
         System.out.println("5 - Progression");
-        final int item5 = 5;
         System.out.println("6 - Prime");
-        final int item6 = 6;
         System.out.println("0 - Exit");
         Scanner scanner = new Scanner(System.in);
         System.out.print("Your choice: ");
-        while (!scanner.hasNextInt() || !scanner.hasNext("[0123456]")) {
-            System.out.println("Please enter a number from 0 to 6!");
-            scanner.next();
-        }
-        switch (scanner.nextInt()) {
-            case item1 -> Cli.getNameAndGreetings();
-            case item2 -> {
-                Cli.getNameAndGreetings();
-                System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-                Even.even(Cli.getUserName());
 
-            }
-            case item3 -> {
-                Cli.getNameAndGreetings();
-                System.out.println("What is the result of the expression?");
-                Calc.calc(Cli.getUserName());
-            }
-            case item4 -> {
-                Cli.getNameAndGreetings();
-                System.out.println("Find the greatest common divisor of given numbers.");
-                GCD.gcd(Cli.getUserName());
-            }
-            case item5 -> {
-                Cli.getNameAndGreetings();
-                System.out.println("What number is missing in the progression?");
-                Progression.progression(Cli.getUserName());
-            }
-            case item6 -> {
-                Cli.getNameAndGreetings();
-                System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'");
-                Prime.prime(Cli.getUserName());
-            }
-            default -> System.out.println("See you!");
+        switch (scanner.nextLine()) {
+            case "1" -> Cli.getNameAndGreetings();
+            case "2" -> Even.run();
+            case "3" -> Calc.run();
+            case "4" -> GCD.run();
+            case "5" -> Progression.run();
+            case "6" -> Prime.run();
+            case "0" -> System.out.println("See you!");
+            default -> throw new Error("Unknown program number entry");
         }
     }
 }
