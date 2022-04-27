5b6cc2d864759dd855d02951cea9b2fa7b3c62c0
Damir Akhmerov <searchingtalib@yahoo.com>
2022-04-27 18:13:25 +0300

    fix Engine.java

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -3,39 +3,31 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Engine {
-    public static void run(String gameDescription, String[][] dataForQuestion) {
-        getName();
+    public static final int NUMBEROFROUNDS = 3;
+
+    public static void run(String gameDescription, String[][] roundsData) {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+        System.out.println("Hello, " + userName + "!");
         System.out.println(gameDescription);
-        for (String[] strings : dataForQuestion) {
+        for (String[] roundData : roundsData) {
+            String question = roundData[0];
+            String correctAnswer = roundData[1];
             System.out.print("Question: ");
-            System.out.println(strings[0]);
+            System.out.println(question);
             System.out.print("Your answer: ");
-            Scanner scanner = new Scanner(System.in);
-            String answer = scanner.nextLine();
-            if (answer.equals(strings[1])) {
-                Engine.getCorrectAnswer();
+            String userAnswer = scanner.nextLine();
+            if (userAnswer.equals(correctAnswer)) {
+                System.out.println("Correct!");
             } else {
-                Engine.getWrongAnswer(answer, strings[1]);
+                System.out.println("'" + userAnswer + "'"
+                        + " is wrong answer ;(. Correct answer was '" + correctAnswer + "'.");
+                System.out.println("Let's try again, " + userName + "!");
                 return;
             }
         }
         System.out.println("Congratulations, " + userName + "!");
-    }
-    private static String userName;
-    public static  void getName() {
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        Scanner scanner = new Scanner(System.in);
-        userName = scanner.nextLine();
-        System.out.println("Hello, " + userName + "!");
-    }
-
-    public static void getCorrectAnswer() {
-        System.out.println("Correct!");
-    }
-
-    public static void getWrongAnswer(String answer, String result) {
-        System.out.println("'" + answer + "'" + " is wrong answer ;(. Correct answer was '" + result + "'.");
-        System.out.println("Let's try again, " + userName + "!");
     }
 }
