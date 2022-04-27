46563d7a9918b2f3b35f9bd919378fcf846d6c3a
Damir Akhmerov <searchingtalib@yahoo.com>
2022-04-27 18:15:20 +0300

    fix Calc.java

--- src/main/java/hexlet/code/games/Calc.java
+++ src/main/java/hexlet/code/games/Calc.java
@@ -1,40 +1,37 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-
-import java.util.Random;
+import hexlet.code.Utils;
 
 public class Calc {
-    public static  void run() {
-        String gameDescription = "What is the result of the expression?";
-        final int numberOfQuestions = 3;
+    public static final String GAMEDESCRIPTION = "What is the result of the expression?";
+    public static final char[] OPERATORS = {'-', '+', '*'};
+    public static final int MAXNUMTODEFINE = 20;
 
-        String[][] dataForQuestion = new String[numberOfQuestions][2];
-        for (var i = 0; i < numberOfQuestions; i++) {
-            int a = generateRandomNumber();
-            String operator = generateOperator();
-            int b = generateRandomNumber();
-            dataForQuestion[i][0] = a + " " + operator + " " + b;
-            int result = switch (operator) {
-                case "+" -> a + b;
-                case "-" -> a - b;
-                case "*" -> a * b;
-                default -> 0;
-            };
-            dataForQuestion[i][1] = String.valueOf(result);
+    public static void run() {
+        String[][] roundsData = new String[Engine.NUMBEROFROUNDS][2];
+        for (var i = 0; i < Engine.NUMBEROFROUNDS; i++) {
+            roundsData[i] = generateRoundData();
         }
-        Engine.run(gameDescription, dataForQuestion);
-    }
-    public static int generateRandomNumber() {
-        Random random = new Random();
-        final int upperRandomLimit = 20;
-        return random.nextInt(upperRandomLimit) + 1;
+        Engine.run(GAMEDESCRIPTION, roundsData);
     }
 
-    public static String generateOperator() {
-        Random random = new Random();
-        String[] operators = {"-", "+", "*"};
-        int i = random.nextInt(operators.length);
-        return operators[i];
+    public static String[] generateRoundData() {
+        int a = Utils.generateRandomNumber(MAXNUMTODEFINE);
+        char operator = OPERATORS[Utils.generateRandomNumber(OPERATORS.length) - 1];
+        int b = Utils.generateRandomNumber(MAXNUMTODEFINE);
+        String[] roundData = new String[2];
+        roundData[0] = a + " " + operator + " " + b;
+        roundData[1] = String.valueOf(calculate(a, b, operator));
+        return roundData;
+    }
+
+    public static int calculate(int a, int b, char operator) {
+        return switch (operator) {
+            case '+' -> a + b;
+            case '-' -> a - b;
+            case '*' -> a * b;
+            default -> 0;
+        };
     }
 }
