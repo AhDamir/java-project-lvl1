36bcce5d09638ab1d61879dd1c7436be795d3db6
Damir Akhmerov <searchingtalib@yahoo.com>
2022-04-27 18:16:26 +0300

    fix Progression.java

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -1,55 +1,46 @@
 package hexlet.code.games;
 
 import java.util.Arrays;
-import java.util.Random;
-
 import hexlet.code.Engine;
+import hexlet.code.Utils;
 
 public class Progression {
+    public static final String GAMEDESCRIPTION = "What number is missing in the progression?";
+    public static final int UPPERLIMITOFPROGRESSIONLENGTH = 13;
+    public static final int LOWERLIMITOFPROGRESSIONLENGTH = 5;
+    public static final int PROGRESSIONFIRSTNUMBERUPPERLIMIT = 20;
+    public static final int PROGRESSIONSTEPLIMIT = 10;
+
     public static void run() {
-        String gameDescription = "What number is missing in the progression?";
-        final int numberOfQuestions = 3;
-
-        String[][] dataForQuestion = new String[numberOfQuestions][2];
-        for (var i = 0; i < numberOfQuestions; i++) {
-            final int upperLimitOfProgressionLength = 13;
-            final int lowerLimitOfProgressionLength = 5;
-            int progressionLength = generateRandomNumber(lowerLimitOfProgressionLength,
-                    upperLimitOfProgressionLength);
-            int[] progression = new int[progressionLength];
-            final int progressionsFirstNumberUpperRandomLimit = 20;
-            progression[0] = generateRandomNumber(progressionsFirstNumberUpperRandomLimit);
-            final int progressionStepLimit = 10;
-            int progressionStep = generateRandomNumber(progressionStepLimit);
-            for (var j = 1; j < progressionLength; j++) {
-                progression[j] = progression[j - 1] + progressionStep;
-            }
-
-            String[] stringProgression = new String[progressionLength];
-            for (var k = 0; k < progressionLength; k++) {
-                stringProgression[k] = String.valueOf(progression[k]);
-            }
-
-            int numberOfUnknownInProgression = generateRandomNumber(0, progressionLength - 1);
-            String unknown = stringProgression[numberOfUnknownInProgression];
-            stringProgression[numberOfUnknownInProgression] = "..";
-            dataForQuestion[i][0] = Arrays.toString(stringProgression)
-                    .replace("[", "")
-                    .replace("]", "")
-                    .replace(",", "");
-            dataForQuestion[i][1] = unknown;
+        String[][] roundsData = new String[Engine.NUMBEROFROUNDS][2];
+        for (var i = 0; i < Engine.NUMBEROFROUNDS; i++) {
+            roundsData[i] = generateRoundData();
         }
-
-        Engine.run(gameDescription, dataForQuestion);
+        Engine.run(GAMEDESCRIPTION, roundsData);
     }
 
-    public static int generateRandomNumber(int upperRandomLimit) {
-        Random random = new Random();
-        return random.nextInt(upperRandomLimit) + 1;
-    }
-
-    public static int generateRandomNumber(int lowerRandomLimit, int upperRandomLimit) {
-        Random random = new Random();
-        return random.nextInt(upperRandomLimit - lowerRandomLimit + 1) + lowerRandomLimit;
+    public static String[] generateRoundData() {
+        String[] roundData = new String[2];
+        int progressionLength = Utils.generateRandomNumber(LOWERLIMITOFPROGRESSIONLENGTH,
+                UPPERLIMITOFPROGRESSIONLENGTH);
+        int[] progression = new int[progressionLength];
+        progression[0] = Utils.generateRandomNumber(PROGRESSIONFIRSTNUMBERUPPERLIMIT);
+        int progressionStep = Utils.generateRandomNumber(PROGRESSIONSTEPLIMIT);
+        for (var j = 1; j < progressionLength; j++) {
+            progression[j] = progression[j - 1] + progressionStep;
+        }
+        String[] stringProgression = new String[progressionLength];
+        for (var k = 0; k < progressionLength; k++) {
+            stringProgression[k] = String.valueOf(progression[k]);
+        }
+        int numberOfUnknownInProgression = Utils.generateRandomNumber(0, progressionLength - 1);
+        String unknown = stringProgression[numberOfUnknownInProgression];
+        stringProgression[numberOfUnknownInProgression] = "..";
+        roundData[0] = Arrays.toString(stringProgression)
+                .replace("[", "")
+                .replace("]", "")
+                .replace(",", "");
+        roundData[1] = unknown;
+        return roundData;
     }
 }
