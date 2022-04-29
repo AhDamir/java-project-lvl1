fa7e0a684303bb78058b730defcf726fe2f0f653
Damir Akhmerov <searchingtalib@yahoo.com>
2022-04-29 13:09:15 +0300

    fix Prime.java

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -4,27 +4,22 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 public class Prime {
-    public static final String GAMEDESCRIPTION = "Answer 'yes' if given number is prime. Otherwise answer 'no'";
-    public static final int MAXNUMTODEFINE = 101;
-    public static final int MINNUMTODEFINE = 2;
+    private static final String GAME_DESCRIPTION = "Answer 'yes' if given number is prime. Otherwise answer 'no'";
+    private static final int MAX_NUM_TO_DEFINE = 101;
+    private static final int MIN_NUM_TO_DEFINE = 2;
 
     public static void run() {
         String[][] roundsData = new String[Engine.NUMBEROFROUNDS][2];
         for (var i = 0; i < Engine.NUMBEROFROUNDS; i++) {
             roundsData[i] = generateRoundData();
         }
-        Engine.run(GAMEDESCRIPTION, roundsData);
+        Engine.run(GAME_DESCRIPTION, roundsData);
     }
 
     public static String[] generateRoundData() {
         String[] roundData = new String[2];
-        int question = Utils.generateRandomNumber(MINNUMTODEFINE, MAXNUMTODEFINE);
-        String correctAnswer;
-        if (isPrime(question)) {
-            correctAnswer = "yes";
-        } else {
-            correctAnswer = "no";
-        }
+        int question = Utils.generateRandomNumber(MIN_NUM_TO_DEFINE, MAX_NUM_TO_DEFINE);
+        String correctAnswer = isPrime(question) ? "yes" : "no";
         roundData[0] = String.valueOf(question);
         roundData[1] = correctAnswer;
         return roundData;
