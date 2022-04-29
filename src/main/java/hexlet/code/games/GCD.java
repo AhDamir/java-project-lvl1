3a8dc6452a32b36972f7312e388faeb145142ad3
Damir Akhmerov <searchingtalib@yahoo.com>
2022-04-29 13:08:10 +0300

    fix GCD.java

--- src/main/java/hexlet/code/games/GCD.java
+++ src/main/java/hexlet/code/games/GCD.java
@@ -4,22 +4,28 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 public class GCD {
-    public static final String GAMEDESCRIPTION = "Find the greatest common divisor of given numbers.";
-    public static final int MAXNUMTODEFINE = 100;
+    private static final String GAME_DESCRIPTION = "Find the greatest common divisor of given numbers.";
+    private static final int MAX_NUM_TO_DEFINE = 100;
 
     public static void run() {
         String[][] roundsData = new String[Engine.NUMBEROFROUNDS][2];
         for (var i = 0; i < Engine.NUMBEROFROUNDS; i++) {
             roundsData[i] = generateRoundData();
         }
-        Engine.run(GAMEDESCRIPTION, roundsData);
+        Engine.run(GAME_DESCRIPTION, roundsData);
     }
 
     public static String[] generateRoundData() {
         String[] roundData = new String[2];
-        int a = Utils.generateRandomNumber(MAXNUMTODEFINE);
-        int b = Utils.generateRandomNumber(MAXNUMTODEFINE);
+        int a = Utils.generateRandomNumber(MAX_NUM_TO_DEFINE);
+        int b = Utils.generateRandomNumber(MAX_NUM_TO_DEFINE);
         roundData[0] = a + " " + b;
+        int gCD = calculateGCD(a, b);
+        roundData[1] = String.valueOf(gCD);
+        return roundData;
+    }
+
+    public static int calculateGCD(int a, int b) {
         int result = 0;
         if (a == b) {
             result = a;
@@ -30,7 +36,6 @@ public class GCD {
                 b = result;
             }
         }
-        roundData[1] = String.valueOf(result);
-        return roundData;
+        return result;
     }
 }
