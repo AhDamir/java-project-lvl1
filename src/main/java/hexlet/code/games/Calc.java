2abb55f25b0c090f1b9ac961284951b5fecdd5de
Damir Akhmerov <searchingtalib@yahoo.com>
2022-04-29 13:07:38 +0300

    fix Calc.java

--- src/main/java/hexlet/code/games/Calc.java
+++ src/main/java/hexlet/code/games/Calc.java
@@ -4,22 +4,22 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 public class Calc {
-    public static final String GAMEDESCRIPTION = "What is the result of the expression?";
-    public static final char[] OPERATORS = {'-', '+', '*'};
-    public static final int MAXNUMTODEFINE = 20;
+    private static final String GAME_DESCRIPTION = "What is the result of the expression?";
+    private static final char[] OPERATORS = {'-', '+', '*'};
+    private static final int MAX_NUM_TO_DEFINE = 20;
 
     public static void run() {
         String[][] roundsData = new String[Engine.NUMBEROFROUNDS][2];
         for (var i = 0; i < Engine.NUMBEROFROUNDS; i++) {
             roundsData[i] = generateRoundData();
         }
-        Engine.run(GAMEDESCRIPTION, roundsData);
+        Engine.run(GAME_DESCRIPTION, roundsData);
     }
 
     public static String[] generateRoundData() {
-        int a = Utils.generateRandomNumber(MAXNUMTODEFINE);
+        int a = Utils.generateRandomNumber(MAX_NUM_TO_DEFINE);
         char operator = OPERATORS[Utils.generateRandomNumber(OPERATORS.length) - 1];
-        int b = Utils.generateRandomNumber(MAXNUMTODEFINE);
+        int b = Utils.generateRandomNumber(MAX_NUM_TO_DEFINE);
         String[] roundData = new String[2];
         roundData[0] = a + " " + operator + " " + b;
         roundData[1] = String.valueOf(calculate(a, b, operator));
